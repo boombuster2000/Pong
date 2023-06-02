e6885bdfa1314cf5f3abbae244d06d94a3511f08
Ethan <chanuka.williams@outlook.com>
2023-06-03 00:17:25 +0100

    Can play again and states which side wins

--- main.cpp
+++ main.cpp
@@ -2,11 +2,27 @@
 
 enum Sides {
 LEFT = 0,
-RIGHT = 1
+RIGHT = 1,
+NONE = -1
+};
+
+struct Text {
+	const char* text;
+	int fontSize;
+	int textSize = MeasureText(text, fontSize);
+
+	void Render(Vector2 position, Color colour) {
+		DrawText(text, position.x - textSize / 2, position.y - fontSize / 2, fontSize, colour);
+	}
 };
 
 struct Speed {
 	float x, y;
+};
+
+struct Win {
+	const char* text;
+	const Sides side;
 };
 
 struct Size {
@@ -19,14 +35,16 @@ private:
 	int m_points = 0;
 	float m_speed = 300;
 	Vector2 m_position;
+	Vector2 m_originalPosition;
 	Color m_colour = WHITE;
-	Size m_size = { 10, 100};
+	Size m_size = { 10, 100 };
 	struct { KeyboardKey up, down; } m_controls;
 
 public:
 	Paddle(Sides side, Vector2 position) {
 		m_side = side;
 		m_position = position;
+		m_originalPosition = position;
 
 		if (side == LEFT) {
 			m_controls.up = KEY_W;
@@ -42,6 +60,10 @@ public:
 		return Rectangle{ m_position.x - (m_size.width / 2), m_position.y - (m_size.height / 2), m_size.width, m_size.height };
 	}
 
+	void ResetPosition() {
+		m_position = m_originalPosition;
+	}
+	
 	Vector2 GetPosition() {
 		return m_position;
 	}
@@ -49,6 +71,11 @@ public:
 	Sides GetSide() {
 		return m_side;
 	}
+
+	void AddPoint() {
+		m_points++;
+	}
+
 
 	int GetPoints() {
 		return m_points;
@@ -75,13 +102,18 @@ public:
 class Ball {
 private:
 	const float m_radius = 5; 
-	Speed m_speed = {300, 300};
+	Speed m_speed;
+	Speed m_originalSpeed;
 	Color m_colour = WHITE;
 	Vector2 m_position;
+	Vector2 m_originalPosition;
 
 public:
-	Ball(Vector2 position) {
+	Ball(Vector2 position, Speed speed) {
 		m_position = position;
+		m_originalPosition = position;
+		m_speed = speed;
+		m_originalSpeed = speed;
 	}
 
 	Vector2 GetPosition() {
@@ -110,8 +142,12 @@ public:
 	void Render() {
 		DrawCircle((int) m_position.x, (int) m_position.y, m_radius, m_colour);
 	}
-};
 
+	void Reset() {
+		m_speed = m_originalSpeed;
+		m_position = m_originalPosition;
+	}
+};
 
 void checkCollitions(Ball* ball, Paddle leftPaddle, Paddle rightPaddle) {
 	const Vector2 ballPosition = (*ball).GetPosition();
@@ -147,6 +183,20 @@ void checkCollitions(Ball* ball, Paddle leftPaddle, Paddle rightPaddle) {
 	}
 }
 
+Win checkWin(Ball ball) {
+	const Vector2 ballPosition = ball.GetPosition();
+
+	if (ballPosition.x <= 0) {
+		return Win{ "Right Side Wins!", RIGHT };
+	}
+	else if (ballPosition.x >= GetScreenWidth()) {
+		return Win{ "Left Side Wins!", LEFT };
+	}
+	else {
+		return Win{ nullptr, NONE };
+	}
+}
+
 
 int main() {
 	InitWindow(800, 600, "Pong");
@@ -156,20 +206,48 @@ int main() {
 
 	Paddle leftPaddle(LEFT, {50, screenCentre.y});
 	Paddle rightPaddle(RIGHT, { GetScreenWidth() - 50.0F, screenCentre.y });
-	Ball ball(screenCentre);
+	Ball ball(screenCentre, Speed {300,300});
+
+	bool won = false;
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		checkCollitions(&ball, leftPaddle, rightPaddle);
-		ball.Move();
-		leftPaddle.Move();
-		rightPaddle.Move();
+		if (!won) {
+			checkCollitions(&ball, leftPaddle, rightPaddle);
+			ball.Move();
+			leftPaddle.Move();
+			rightPaddle.Move();
+		}
 
 		ball.Render();
 		leftPaddle.Render();
 		rightPaddle.Render();
+
+		Win win = checkWin(ball);
+
+		if (win.side != NONE and win.text != NULL) {
+			won = true;
+
+			if (win.side == leftPaddle.GetSide()) leftPaddle.AddPoint();
+			else if (win.side == rightPaddle.GetSide()) rightPaddle.AddPoint();
+
+			Text winText = {win.text, 60};
+			winText.Render(screenCentre, YELLOW);
+
+			Text playAgainText = { "Press space to continue", 25 };
+			playAgainText.Render({ screenCentre.x, screenCentre.y + 50 }, YELLOW);
+
+			// Reset Game
+			if (IsKeyPressed(KEY_SPACE)) {
+				ball.Reset();
+				leftPaddle.ResetPosition();
+				rightPaddle.ResetPosition();
+				win.text = nullptr;
+				won = false;
+			}
+		}
 
 		DrawFPS(5, 5);
 		EndDrawing();
